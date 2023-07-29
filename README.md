4463f7fadcc62ad23b5885e8f2797dc7f04daf56
R0GUEEE <dillinv@live.com>
2023-07-29 15:03:58 -0400

    Init

---
+++ README.md
@@ -0,0 +1,10 @@
+# Android device tree for samsung SM-T220 (gta7litewifi)
+
+```
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+```
---
+++ twrp_gta7litewifi.mk
@@ -0,0 +1,28 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Inherit from gta7litewifi device
+$(call inherit-product, device/samsung/gta7litewifi/device.mk)
+
+PRODUCT_DEVICE := gta7litewifi
+PRODUCT_NAME := twrp_gta7litewifi
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-T220
+PRODUCT_MANUFACTURER := samsung
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung-ss
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="gta7litewifixx-user 12 SP1A.210812.016 T220XXU2CWB3 release-keys"
+
+BUILD_FINGERPRINT := samsung/gta7litewifixx/gta7litewifi:12/SP1A.210812.016/T220XXU2CWB3:user/release-keys
