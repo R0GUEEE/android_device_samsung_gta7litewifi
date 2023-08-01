882010e503da4ab1c09f018361246cdfb58f8740
R0GUEEE <41440185+R0GUEEE@users.noreply.github.com>
2023-07-31 22:03:39 -0400

    Update twrp_gta7litewifi.mk

--- twrp_gta7litewifi.mk
+++ twrp_gta7litewifi.mk
@@ -1,30 +1,42 @@
 #
-# Copyright (C) 2023 The Android Open Source Project
-# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+# Copyright (C) 2020 The Android Open Source Project
 #
-# SPDX-License-Identifier: Apache-2.0
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
 
-# Inherit from those products. Most specific first.
+# Release name
+PRODUCT_RELEASE_NAME := gta7litewifi
+
+# Inherit from common AOSP config
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
-# Inherit some common twrp stuff.
+# Inherit some common TWRP stuff.
+# For PitchBlack use vendor/pb/config/common.mk
 $(call inherit-product, vendor/twrp/config/common.mk)
 
-# Inherit from gta7litewifi device
+# Inherit device configuration
 $(call inherit-product, device/samsung/gta7litewifi/device.mk)
 
-PRODUCT_DEVICE := gta7litewifi
-PRODUCT_NAME := twrp_gta7litewifi
-PRODUCT_BRAND := samsung
-PRODUCT_MODEL := SM-T220
-PRODUCT_MANUFACTURER := samsung
-
-PRODUCT_GMS_CLIENTID_BASE := android-samsung-ss
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="gta7litewifixx-user 12 SP1A.210812.016 T220XXU2CWB3 release-keys"
-
-BUILD_FINGERPRINT := samsung/gta7litewifixx/gta7litewifi:12/SP1A.210812.016/T220XXU2CWB3:user/release-keys
+# Charger
+PRODUCT_PACKAGES += \
+    charger_res_images
 
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,device/samsung/gta7litewifi/recovery/root,recovery/root)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := twrp_gta7litewifi
+PRODUCT_DEVICE := gta7litewifi
+PRODUCT_MODEL := SM-T220
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
