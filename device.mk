294869dc13b56ee732bd084be5cc650908159150
R0GUEEE <41440185+R0GUEEE@users.noreply.github.com>
2023-08-03 19:08:56 -0400

    Update device.mk

--- device.mk
+++ device.mk
@@ -6,3 +6,12 @@
 #
 
 LOCAL_PATH := device/samsung/gta7litewifi
+
+# Dynamic partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+PRODUCT_SHIPPING_API_LEVEL := 33
+
+# fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd
