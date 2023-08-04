bd028cfd8c1f8a268bb231a58ca12130ed7c5637
R0GUEEE <41440185+R0GUEEE@users.noreply.github.com>
2023-08-03 23:30:14 -0400

    Commit

--- device.mk
+++ device.mk
@@ -7,10 +7,21 @@
 
 LOCAL_PATH := device/samsung/gta7litewifi
 
-# Dynamic partitions
-PRODUCT_USE_DYNAMIC_PARTITIONS := true
+# Rootdir
+PRODUCT_PACKAGES += \
+    init.insmod.sh \
 
 # fastbootd
+TW_INCLUDE_FASTBOOTD := true
+
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.1-impl-mock \
+    android.hardware.fastboot@1.0-impl-mock.recovery
     fastbootd
+
+PRODUCT_PRODUCT_PROPERTIES += \
+	ro.fastbootd.available=true
+
+# Dynamic partitions
+PRODUCT_BUILD_SUPER_PARTITION := true
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
